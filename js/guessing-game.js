0ffe71856d93d30b420784a2d5c73f1789ac6b84
shawns21 <shawnsinanan21@gmail.com>
2023-07-08 12:06:08 -0400

    finished random number and shuffle function

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -5,3 +5,28 @@ by running "npm run test".
 In this file, you will also include the event listeners that are needed to interact with your HTML file when
 a user clicks a button or adds a guess to the input field.
 */
+
+function generateWinningNumber(){
+     return (Math.floor(Math.random() * 100) + 1);
+}
+
+function shuffle(arr){
+
+    for(let i = (arr.length - 1); i > 1; i--){
+
+        let randNum = (Math.floor(Math.random() * (i + 1)));
+
+        let temp = arr[randNum];
+
+        arr[randNum] = arr[i];
+
+        arr[i] = temp;
+
+    }
+
+    return(arr);
+}
+
+let suf = [20, 50, 70];
+
+console.log(shuffle(suf));
