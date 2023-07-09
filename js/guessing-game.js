3b24181145ce316406866995b6b479ce88d77f5c
shawns21 <shawnsinanan21@gmail.com>
2023-07-09 01:03:11 -0400

    finished javascript class code

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -27,6 +27,90 @@ function shuffle(arr){
     return(arr);
 }
 
-let suf = [20, 50, 70];
+class Game {
 
-console.log(shuffle(suf));
+    playersGuess = null;
+
+    pastGuesses = [];
+
+    winningNumber = generateWinningNumber();
+
+    difference() {
+        return Math.abs(this.playersGuess - this.winningNumber);
+    }
+
+    isLower() {
+        if (this.playersGuess < this.winningNumber){
+            return true;
+        }
+
+        else{
+            return false;
+        }
+    }
+    
+
+    playersGuessSubmission(num) {
+        this.playersGuess = num
+
+        if (this.playersGuess < 1 || this.playersGuess > 100 || typeof(this.playersGuess) != "number"){
+            throw ("That is an invalid guess.");
+        }
+        
+        return this.checkGuess();
+        
+    }
+
+    checkGuess() {
+
+        if (this.winningNumber === this.playersGuess){
+            return "You Win!";
+        }
+
+        if (this.pastGuesses.length >= 4){
+            return "You Lose.";
+        }
+
+        if (this.pastGuesses.includes(this.playersGuess)){
+            return "You have already guessed that number.";
+        }
+
+         this.pastGuesses.push(this.playersGuess);
+
+        if (this.difference() < 10){
+            return "You're burning up!";
+        }
+
+        if (this.difference() < 25){
+            return "You're lukewarm.";
+        }
+        
+        if (this.difference() < 50){
+            return "You're a bit chilly.";
+        }
+
+        if (this.difference() < 100){
+            return "You're ice cold!";
+        }
+    }
+    
+    provideHint() {
+
+        let hints = [];
+        
+        hints.push(generateWinningNumber());
+
+        hints.push(generateWinningNumber());
+
+        hints.push(this.winningNumber);
+
+        shuffle(hints);
+
+        return hints;
+    }
+
+}
+
+function newGame() {
+        return new Game();
+    }
